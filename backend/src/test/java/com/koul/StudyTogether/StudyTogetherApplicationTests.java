9e16362ee41c398442e2b0acff7a301a31d3d321
Koul <dongik0122@gmail.com>
2025-11-24 23:10:34 +0900

    chore: 도커 컴포즈, appliction.yml 작성
    - PostgreSQL Docker 구성
    - Spring Boot 기본 설정
    - env 작성

---
+++ backend/src/test/java/com/koul/StudyTogether/StudyTogetherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koul.StudyTogether;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyTogetherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
